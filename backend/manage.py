1b05a8df85121500e59cca121b134c2d4353589c
Rajeev Swami <rajeevswami056@gmail.com>
2026-07-31 17:22:44 +0530

    Use production settings in manage.py

--- backend/manage.py
+++ backend/manage.py
@@ -1,10 +1,9 @@
-#!/usr/bin/env python
 import os
 import sys
 
 
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.development")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -17,3 +16,25 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+# #!/usr/bin/env python
+# import os
+# import sys
+
+
+# def main():
+#     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.development")
+#     try:
+#         from django.core.management import execute_from_command_line
+#     except ImportError as exc:
+#         raise ImportError(
+#             "Couldn't import Django. Is it installed and available on your PYTHONPATH? "
+#             "Did you forget to activate a virtual environment?"
+#         ) from exc
+#     execute_from_command_line(sys.argv)
+
+
+# if __name__ == "__main__":
+#     main()
