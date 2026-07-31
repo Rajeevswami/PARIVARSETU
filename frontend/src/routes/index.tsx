6a74d2634899364d66749bb977151803fa58eeb7
Rajeev Swami <rajeevswami056@gmail.com>
2026-07-31 21:43:31 +0530

    Fix Vercel TypeScript build

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import {Route, Routes } from "react-router-dom";
 
 import { MainLayout } from "@/layouts/MainLayout";
 import { AcceptInvitationPage } from "@/pages/AcceptInvitationPage";
